a9ff14d5b1ae05bf2ad4d1a43914d07e85dbcaff
yfor <yfor1008@gmail.com>
2020-08-27 13:40:30 +0800

    [update] update visualization of histeq

--- histeq/calc_T.m
+++ histeq/calc_T.m
@@ -28,18 +28,20 @@ for i = 1:grayLevel
 end
 
 if show
-    show_process(hist, eq, cdf);
+    ranges = getRanges(hist, 0.4);
+    show_process(hist, eq, cdf, ranges);
 end
 
 end
 
-function show_process(hist, eq, cdf)
+function show_process(hist, eq, cdf, ranges)
 % show_process - 显示变换过程
 %
 % input:
 %   - hist: n*1, 直方图, n为图像灰度个数
 %   - eq: n*1, 调整后的直方图
 %   - cdf: n*1, 累计直方图
+%   - ranges: 2*1, 动态范围, [index_low, index_up]
 %
 
 figure('NumberTitle', 'off', 'Name', 'Porcess of Histogram Transform')
@@ -48,21 +50,27 @@ T = tiledlayout(2,2);
 colors = {'r', 'g', 'b', 'c'};
 
 grayVal = 1 : length(hist);
+grayVal = grayVal';
+
+CDF = fix(cdf*(length(hist)-1))+1;
+RANGES(1) = CDF(ranges(1));
+RANGES(2) = CDF(ranges(2));
 
 nexttile(1)
-barh(grayVal', eq, 'FaceColor', colors{2}, 'BarWidth', 1.0);
+barh(grayVal, eq, 'FaceColor', colors{2}, 'EdgeColor', colors{2}, 'BarWidth', 1.0);
 set(gca, 'color', 'none'); % set background
 set(gca,'XDir','reverse');
 ylim([0, length(hist)])
 
 nexttile(2)
-plot(grayVal', fix(cdf*(length(hist)-1))+1, 'color', colors{4}, 'linewidth', 1.1);
-axis([0, length(hist) 1 length(hist)])
+plot(grayVal, CDF, 'color', colors{4}, 'linewidth', 1.1);
+axis([0, length(hist) 0 length(hist)])
 set(gca, 'color', 'none'); % set background
 
 nexttile(4)
-bar(grayVal', hist, 'FaceColor', colors{3}, 'BarWidth', 1.0);
+bar(grayVal, hist, 'FaceColor', colors{3}, 'EdgeColor', colors{3}, 'BarWidth', 1.0);
 set(gca, 'color', 'none'); % set background
+set(gca,'YDir','reverse');
 hold on,
 
 xlim([0, length(hist)])
@@ -77,5 +85,53 @@ alpha = ones(size(fig_rgb, 1), size(fig_rgb, 2));
 fig_gray = rgb2gray(fig_rgb);
 alpha(fig_gray==240) = 0;
 imwrite(fig_rgb, 'transform.png', 'Alpha', alpha);
+
+figure('NumberTitle', 'off', 'Name', 'Porcess1 of Histogram Transform')
+T = tiledlayout(2,2);
+
+colors = {'r', 'g', 'b', 'c'};
+
+grayVal = 1 : length(hist);
+grayVal = grayVal';
+
+CDF = fix(cdf*(length(hist)-1))+1;
+RANGES(1) = CDF(ranges(1));
+RANGES(2) = CDF(ranges(2));
+
+nexttile(1)
+barh(grayVal, eq, 'FaceColor', colors{2}, 'EdgeColor', colors{2}, 'BarWidth', 1.0);
+hold on,
+barh(grayVal(RANGES(1):RANGES(2)), eq(RANGES(1):RANGES(2)), 'FaceColor', colors{1}, 'EdgeColor', colors{1}, 'BarWidth', 1.0);
+set(gca, 'color', 'none'); % set background
+set(gca,'XDir','reverse');
+ylim([0, length(hist)])
+
+nexttile(2)
+plot(grayVal, CDF, 'color', colors{4}, 'linewidth', 1.1);
+hold on,
+plot(grayVal(ranges(1):ranges(2)), CDF(ranges(1):ranges(2)), 'color', colors{1}, 'linewidth', 1.1);
+axis([0, length(hist) 0 length(hist)])
+set(gca, 'color', 'none'); % set background
+
+nexttile(4)
+bar(grayVal, hist, 'FaceColor', colors{3}, 'EdgeColor', colors{3}, 'BarWidth', 1.0);
+hold on,
+bar(grayVal(ranges(1):ranges(2)), hist(ranges(1):ranges(2)), 'FaceColor', colors{1}, 'EdgeColor', colors{1}, 'BarWidth', 1.0);
+set(gca, 'color', 'none'); % set background
+set(gca,'YDir','reverse');
+hold on,
+
+xlim([0, length(hist)])
+
+T.TileSpacing = 'compact';
+T.Padding = 'compact';
+
+set(gca, 'color', 'none');
+fig_rgb = getframe(gcf);
+fig_rgb = fig_rgb.cdata;
+alpha = ones(size(fig_rgb, 1), size(fig_rgb, 2));
+fig_gray = rgb2gray(fig_rgb);
+alpha(fig_gray==240) = 0;
+imwrite(fig_rgb, 'transform1.png', 'Alpha', alpha);
 
 end
