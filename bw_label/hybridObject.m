17e941f9efdf535d064a077618741710e5fd92ad
yfor <yfor1008@gmail.com>
2020-11-06 18:24:45 +0800

    [feat] add hybrid object method for CCL

---
+++ bw_label/hybridObject.m
@@ -0,0 +1,68 @@
+function [labeled] = hybridObject(bw)
+% contourTrace - 区域连通, 基于迭代方法
+%
+% input:
+%   - bw: H*W, 二值图像, 1 为前景, 0 为背景
+% output:
+%   - labeled: H*W, 标注后的图像, 每一个连通区域内的像素值相同
+%
+% docs:
+%   1. 基于迭代方法的原理详见: "Hybrid object labelling in digital images"
+%
+
+[H, W] = size(bw);
+
+labeled = zeros(H, W);
+label = 0;
+
+for r = 1:H
+    for c = 1:W
+
+        if bw(r, c)
+            label = label + 1;
+            labeled = Label(bw, labeled, label, H, W, c, r);
+        end
+
+    end
+end
+
+end
+
+function [labeled] = Label(bw, labeled, label, imH, imW, x, y)
+% Label - 标注函数
+%
+% input:
+%   - bw: 二值图像
+%   - labeled: 标注图像
+%   - label: 当前轮廓标签
+%   - imH: 图像高度
+%   - imW: 图像宽度
+%   - x: 坐标
+%   - y: 坐标
+% output:
+%   - labeled: 标注后图像
+%
+
+m = x;
+
+while (x - 1 > 1) && bw(y, x - 1)
+    x = x - 1;
+    labeled(y, x) = label;
+end
+
+while (m <= imW) && bw(y, m)
+    labeled(y, m) = label;
+    m = m + 1;
+end
+
+while x < m
+    if (y - 1 > 1) && bw(y - 1, x)
+        labeled = Label(bw, labeled, label, imH, imW, x, y - 1);
+    end
+    if (y + 1 <= imH) && bw(y + 1, x)
+        labeled = Label(bw, labeled, label, imH, imW, x, y + 1);
+    end
+    x = x + 1;
+end
+
+end
--- bw_label/main_test.m
+++ bw_label/main_test.m
@@ -2,7 +2,7 @@ close all; clear; clc;
 
 
 I = imread('rice.png');
-I = imresize(I, 3);
+% I = imresize(I, 3);
 % figure, imshow(I)
 % title('Synthetic Image')
 
@@ -10,20 +10,26 @@ BW = I > 128;
 figure, imshow(BW)
 title('Binary Image')
 
-% matlab test
-labeled = bwlabel(BW);
-labeled = label2rgb(labeled, 'jet', 'k', 'shuffle');
-figure, imshow(labeled)
-title('Label Image')
+% % matlab test
+% labeled = bwlabel(BW);
+% labeled = label2rgb(labeled, 'jet', 'k', 'shuffle');
+% figure, imshow(labeled)
+% title('Label Image')
 
-% seedfill
-labeled1 = seedFill(BW);
-labeled11 = label2rgb(labeled1, 'jet', 'k', 'shuffle');
-figure, imshow(labeled11)
-title('Label Image')
+% % seedfill
+% labeled1 = seedFill(BW);
+% labeled11 = label2rgb(labeled1, 'jet', 'k', 'shuffle');
+% figure, imshow(labeled11)
+% title('Label Image')
 
-% contourtrace
-labeled2 = contourTrace(BW);
-labeled22 = label2rgb(labeled2, 'jet', 'k', 'shuffle');
-figure, imshow(labeled22)
+% % contourtrace
+% labeled2 = contourTrace(BW);
+% labeled22 = label2rgb(labeled2, 'jet', 'k', 'shuffle');
+% figure, imshow(labeled22)
+% title('Label Image')
+
+% hybridobject
+labeled3 = hybridObject(BW);
+labeled33 = label2rgb(labeled3, 'jet', 'k', 'shuffle');
+figure, imshow(labeled33)
 title('Label Image')
